e75da3772ee6da821d230b0c0ce8e559031fcdbe
Cygran <apcanning90@gmail.com>
2025-02-12 01:12:06 +0000

    Updated some tags in super constructor for ParentNode after inital testing

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -33,7 +33,7 @@ class LeafNode(HTMLNode):
     
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
-        super().__init__(tag, children, props=props)
+        super().__init__(tag=tag, children=children, props=props)
     def to_html(self):
         if self.tag is None:
             raise ValueError("No Tag Value in Parent")
